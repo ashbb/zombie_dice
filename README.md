efc2e68dad977d5afd286aaab88ede28bbf16bd2
ashbb <ashbbb@gmail.com>
2011-06-28 23:03:11 +0900

    Oops, I've misunderstood the game rules. So, rewrote the code from scratch.

---
+++ README.md
@@ -0,0 +1,39 @@
+Zombie Dice on Green Shoes
+=====================
+
+[Zombie Dice](http://www.sjgames.com/dice/zombiedice/) is a famous tabletop game which was made by Steve Jackson Games.
+
+I learned about it in the forum at [RubyLearning.org](http://rubylearning.org). It was introduced by **April Burba**. 
+
+He said that he ported it to a computer game which was a text based game in Ruby. So cool!
+
+I was inspired to port it in Green Shoes. :)
+
+
+Snapshot
+---------
+![zombie-dice-on-green-shoes-v0.1.png](http://github.com/ashbb/zombie_dice/raw/master/zombie-dice-on-green-shoes-v0.1.png)
+
+![zombie-dice-shot-gunned.png](http://github.com/ashbb/zombie_dice/raw/master/zombie-dice-shot-gunned.png)
+
+
+How to play Zombie Dice?
+------------------------
+Look at [a Streaming Flash Video](http://www.rin-shun.com/shoes/zombie_dice.swf.html)
+
+
+Game Rules
+-----------
+Download the rules in PDF form in [English](http://www.sjgames.com/dice/zombiedice/img/ZDRules_English.pdf).
+
+
+Dice
+----
+There are 3 red dice, 4 yellow dice and 6 green dice.
+
+- Red dice sides: 'BANG','BANG','BANG','Run!','Run!','Brainnns'
+
+- Yellow Dice Sides: 'Run!','BANG','Brainnns','BANG','Brainnns','Run!'
+
+- Green Dice Sides: 'Brainnns','Brainnns','Brainnns','Run!','Run!','BANG'
+
--- zombie_dice_on_green_shoes.rb
+++ zombie_dice_on_green_shoes.rb
@@ -1,13 +1,44 @@
 require 'green_shoes'
 
-Die = Struct.new :img, :color, :side
+Die = Struct.new :img, :color, :side, :imgs
 Player = Struct.new :name, :score, :bangs, :brains, :runs
 DICE, WIN, GUNNED = [], [], []
-BRAINS, BANGS, RUNS = [50, 100], [400, 100], [230, 350]
+BRAINS, BANGS, TAKES, RUNS = [50, 100], [350, 100], [350, 200], [160, 380]
+RED = [%w[brain run run bang bang bang], :red, 3]
+YELLOW = [%w[brain brain run run bang bang], :yellow, 4]
+GREEN = [%w[brain brain brain run run bang], :green, 6]
 
-Shoes.app title: 'Zombie Dice' do
-  def roll
+class Array
+  def method_missing m
+    self.each &m
+  end
+end
+
+class Die
+  def shake
+    img.hide
+    self.img, self.side = imgs[rand 6]
+  end
+end
+
+class Shoes
+  class Button
+    def place top = 200
+      tap{|s| s.hide.move(450, top).style width: 100, height: 100}
+    end
+  end
+end
+
+Shoes.app title: 'Zombie Dice v0.1' do
+  def take
     (3 - @player.runs.length).times{@player.runs << @dice.pop}
+    show_dice @player.runs, TAKES, :vertical
+    @take.hide
+    @roll.show
+  end
+
+  def roll
+    @player.runs.shake
     show_dice @player.runs, RUNS
     @check.show
     @roll.hide
@@ -29,79 +60,76 @@ Shoes.app title: 'Zombie Dice' do
     show_dice @player.runs, RUNS
     @check.hide
     if @player.runs.length == 3
-      alert 'Oh, Three Footprints. ROLL again.'
+      alert 'Oh, Three Footprints. SHAKE&TAKE again.'
       @player.runs.each{|d| d.img.hide}
       @player.runs = []
-      @roll.show
+      @take.show
       return
     end
-    if @player.bangs.length > 2
-      GUNNED.each &:show
-    else
-      [@ss, @kg].each &:show
-    end
+    @player.bangs.length > 2 ? GUNNED.show : [@ss, @kg].show
   end
 
-  def show_dice dice, area
+  def show_dice dice, area, flag = nil
     dice.each_with_index do |d, i|
-      d.img.show.move area[0] + i * 55, area[1]
+      i, j = flag ? [0, i] : [i%3, i/3]
+      d.img.show.move area[0] + i * 55, area[1] + j * 55
     end
   end
 
   def make_score name
     @players.map do |u|
-      line = "%10s: %2d Brains\n" % [u.name, u.score]
+      line = "%10s\t: %2d Brains\n" % [u.name, u.score]
       u.name == name ? bg(line, yellow) : line
     end.join
   end
 
   def turn_next_player first = nil
     @dice = DICE.sort_by{rand}
-    if first
-      @player = @players[0]
+    (@player = @players[0]; return) if first
+    DICE.each{|d| d.img.hide}
+    [@ss, @kg, @hand].hide
+    @player.score += @player.brains.length
+    if @player.score > 12
+      @score.text = make_score @player.name
+      WIN.show
     else
-      DICE.each{|d| d.img.hide}
-      [@ss, @kg].each &:hide
-      @player.score += @player.brains.length
-      (WIN.each &:show; return) if @player.score > 12
       @player = @players[(@players.index(@player) + 1) % @players.length]
       @score.text = make_score @player.name
       @player.bangs, @player.brains, @player.runs = [], [], []
-      @roll.show
+      [@hand, @take].show
     end
   end
 
-  background tan
+  background dimgray
   nostroke
-  @players = %w[ashbb kira].map{|name| Player.new name, 0, [], [], []}
+  @hand = rect TAKES[0]-10, TAKES[1]-10, 220, 250, curve: 10, fill: gray, hidden: true
+  @players = %w[ashbb April].map{|name| Player.new name, 0, [], [], []}
   @score = para make_score @players.first.name
 
-  copy = [1, 3, 2, 3, 1, 2, 2, 2, 2]
-  Dir['./dice/*.png'].each_with_index do |file, i|
-    color, side = File.basename(file, '.png').split('_')
-    copy[i].times{DICE << Die.new(image(file, hidden: true), color, side.to_sym)}
+  [RED, YELLOW, GREEN].each do |sides, color, n|
+    n.times do
+      imgs = sides.map{|side| [image("./dice/#{color}_#{side}.png", hidden: true), side.to_sym]}
+      i = rand 6
+      DICE << Die.new(imgs[i][0], color, sides[i].to_sym, imgs)
+    end
   end
 
-  @roll = button("SHAKE\nTAKE\nROLL"){roll}
-  @roll.hide.move(500, 300).style width: 80, height: 60
-  @check = button('CHECK'){check}.hide
-  @check.move(500, 400).style width: 80
+  @take = button("SHAKE & TAKE"){take}.place
+  @roll = button("ROLL"){roll}.place
+  @check = button('CHECK'){check}.place
+  @ss = button('Stop and Score'){turn_next_player}.place 330
+  @kg = button('Keep Going'){[@ss, @kg].hide; show_dice @player.runs, TAKES, :vertical; @take.show}.place
 
-  @ss = button('Stop and Score'){turn_next_player}
-  @ss.hide.move(100, 450).style width: 100
-  @kg = button('Keep Going'){[@ss, @kg].each &:hide; @roll.show}
-  @kg.hide.move(400, 450).style width: 100
-
-  GUNNED << rect(100, 120, 400, 300, curve: 20, fill: rgb(183, 0, 0))
+  GUNNED << rect(100, 120, 400, 300, curve: 20, fill: rgb(183, 0, 0, 0.7))
   GUNNED << para('You got', left: 150, top: 170)
   GUNNED << title('SHOT GUNNED!', left: 150, top: 220)
   GUNNED << button("next player's turn"){
     @player.brains = []
-    GUNNED.each &:hide
+    GUNNED.hide
     turn_next_player
   }
   GUNNED.last.move(350, 350).style width: 130
-  GUNNED.each &:hide
+  GUNNED.hide
 
   WIN << rect(100, 120, 400, 300, curve: 20, fill: white.push(0.4))
   WIN << title('YOU WIN!!', left: 150, top: 220)
@@ -109,16 +137,16 @@ Shoes.app title: 'Zombie Dice' do
   WIN.last.move(400, 350).style width: 50
   WIN << button('Start Over'){
     @players.each{|p| p.score, p.bangs, p.brains, p.runs = 0, [], [], []}
-    WIN.each &:hide
+    WIN.hide
     DICE.each{|d| d.img.hide}
     turn_next_player
   }
   WIN.last.move(150, 350).style width: 150
-  WIN.each &:hide
+  WIN.hide
 
   opening = []
   opening << para(strong("Zombie\nDice"), left: 100, top: 100, size: 72)
-  opening << button('Start'){opening.clear; @roll.show}
+  opening << button('Start'){opening.clear; [@hand, @take].show}
   opening.last.move(100, 400).style width: 50
   opening << button('Exit'){close}
   opening.last.move(450, 400).style width: 50
